bfbc7aca8a42835758a20a3cbdeb7238daf217d5
Stian Karlsen <stiankarlsen709@gmail.com>
2026-03-24 16:00:46 +0100

    counter week 3

--- src/components/week3/counter.tsx
+++ src/components/week3/counter.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Counter(){
-const [count, setCount] = useState(0);
-    return(
-        <>
-        <div className="counterWrapper">
-            <p>{count}</p>
-            <button onClick={() => {setCount(count = 0)}}>Add</button>
-            <button onClick={() => {setCount(count + 1)}}>Add</button>
-            <button onClick={() => {setCount(count - 1)}}>Delite</button>
-        </div>
-        </>
-    )
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="counterWrapper">
+        <p>{count}</p>
+        <button
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          Delite
+        </button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </>
+  );
 }
